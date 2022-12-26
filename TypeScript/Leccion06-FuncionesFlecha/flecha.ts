76337ac9d1e2063dac8e84d38c13d5e6c65bb274
GandyA23 <avila.gandy@gmail.com>
2022-12-26 14:24:05 -0600

    Sección 246 del curso finalizada.
    246. Funciones de Flecha con TypeScript.

---
+++ TypeScript/Leccion06-FuncionesFlecha/flecha.ts
@@ -0,0 +1,11 @@
+// Función normal
+const suma = function(a: number, b: number) : number
+{
+    return a + b;
+}
+
+// Función de flecha
+const sumaFlecha = (a: number, b: number) => a + b;
+
+console.log(suma(1, 2));
+console.log(sumaFlecha(3, 4));
