accdb930a827ca7be9890d5f3c097e9a71efff11
GandyA23 <avila.gandy@gmail.com>
2022-12-26 13:55:01 -0600

    Sección 244 del curso finalizada
    244. Definición de Generics con TypeScript.

---
+++ TypeScript/Leccion04-Generics/generics.ts
@@ -0,0 +1,13 @@
+// Declaración de un arreglo de números
+let arregloNumeros: Array<number>;
+
+arregloNumeros = [1, 2, 3, 4, 5];
+
+// Generá un error, debido a que el arreglo no acepta strings
+// arregloNumeros = ["Gandy", "Esaú"];
+
+// Imprime todo el arreglo
+console.log(arregloNumeros);
+
+// Imprime una posición
+console.log(arregloNumeros[0]);
